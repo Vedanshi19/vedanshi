1f9e598edd723eeed3dd329e7a549e05a5050953
Vedanshi19 <vedanshipv19@gmail.com>
2024-07-30 14:59:33 +0530

    added sections and their styling

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import styles from './ProjectsStyles.module.css';
+import viberr from '../../assets/CryptoCurrency.png';
+import freshBurger from '../../assets/Weather2.png';
+import hipsster from '../../assets/background.png';
+import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={viberr}
+          link="https://github.com/Vedanshi19/Cryptocurrency-Website"
+          h3="CryptoCurrency"
+          p="Cryptocurrency Price"
+        />
+        <ProjectCard
+          src={freshBurger}
+          link="https://github.com/Vedanshi19/Weather-App"
+          h3="Weather App"
+          p="Weather conditions"
+        />
+        <ProjectCard
+          src={hipsster}
+          link="https://github.com/Vedanshi19/bgChanger"
+          h3="BgChanger"
+          p="Change color theme"
+        />
+      </div>
+    </section>
+  );
 }
 
-export default Projects
+export default Projects;
